dc1e6bacacbf2e5d1e5170ee74a84ad3fb470c3d
mac <mac@Aleks-MacBook-Pro.local>
2019-02-22 16:46:29 -0500

    express server setup with users, posts, and profile route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://alek:alek@devconnector-zmvdg.mongodb.net/test?retryWrites=true'
+};
---
+++ routes/api/profiles.js
@@ -0,0 +1,11 @@
+// Deals with profile info such as bio, education, etc.
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/profile/test
+// @desc    Tests profiles route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: "Profile works" })
+);
+
+module.exports = router;
