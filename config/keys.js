f4402e7bb368fb1f63967ecb28d25dd031049a2d
mac <mac@Aleks-MacBook-Pro.local>
2019-02-27 15:49:59 -0500

    Added JWT generation on login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://alek:alek@devconnector-zmvdg.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://alek:alek@devconnector-zmvdg.mongodb.net/test?retryWrites=true',
+    secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load user model
 const User = require('../../models/User');
@@ -71,7 +72,20 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if(isMatch) {
-            res.json({msg: 'Success'})
+            // Password is correct
+            const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create JWT Payload
+
+            // Sign the token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: 'true',
+                  token: 'Bearer ' + token
+                })
+            });
           } else {
             return res.status(404).json({password: 'incorrect password'})
           }
