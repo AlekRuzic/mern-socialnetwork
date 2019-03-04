0853f4178d2efbc9411f1ebaf25d37019dd40b48
mac <alekruzic@live.com>
2019-03-04 11:17:27 -0500

    Added routes to view posts and route for users to delete posts that they have made

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 const validatePostInput = require('../../validation/post');
 
 
@@ -10,6 +11,27 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: "Posts works" })
 );
+
+
+// @route   GET api/posts
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: 'No post found with that id'}));
+});
+
+
+// @route   GET api/posts/:id
+// @desc    View posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
 
 
 // @route   POST api/posts
@@ -33,6 +55,25 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   newPost.save().then(post => res.json(post));
 });
 
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check that the post belongs to the logged in user
+          if (post.user.toString() !== req.user.id) {
+            return (res.status(401).json({ notauthorized: 'User not authorized to delete this post '}));
+          }
+
+          post.remove().then(_ => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'The post you are trying to delete could not be found'}))
+    })
+})
 
 
 module.exports = router;
