6c88e8544329ddb4f3bdc86c998cdbe49c428b23
mac <alekruzic@live.com>
2019-03-16 20:23:04 -0400

    react login and register forms set up and working (not with redux yet)

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
 const app = express();
+app.use(cors());
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 
 // DB Config
 const db = require('./config/keys').mongoURI;
