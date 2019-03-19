89870ec5112e1276782946f3f124a43411212f65
mac <alekruzic@live.com>
2019-03-19 16:40:57 -0400

    added ability to properly log in from the front end

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
-import { GET_ERRORS } from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +15,35 @@ export const registerUser = (userData, history) => dispatch => {
           payload: err.response.data
         })
       );
+}
+
+// Login - get user token
+export const loginUser = userData => dispatch => {
+  axios.post('http://localhost:5000/api/users/login', userData)
+  .then(res => {
+    // Save to local storage
+    const { token } = res.data;
+    // Set token to local storage
+    localStorage.setItem('jwtToken', token);
+    // Set token to auth header
+    setAuthToken(token);
+    // Decode token to get user data
+    const decoded = jwt_decode(token);
+    // Set current user
+    dispatch(setCurrentUser(decoded));
+  })
+  .catch(err =>
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+      })
+  );
+}
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
